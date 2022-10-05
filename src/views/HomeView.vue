91f63a77985b277a7acc6390d8c914b365f49e4b
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-05 11:33:25 +0200

    Extract category and word to translate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1>Fruit</h1>
-  <p>Pineapple</p>
+  <h1>{{ category }}</h1>
+  <p>{{ wordToTranslate }}</p>
   <AnswerItems :answers="answers" />
   <p>This was correct</p>
   <button>Next question</button>
@@ -10,8 +10,12 @@
 import AnswerItems from "../components/AnswerItems.vue";
 import { ref, onBeforeMount } from "vue";
 const answers = ref([]);
+const wordToTranslate = ref("");
+const category = ref("");
 
 onBeforeMount(() => {
+  wordToTranslate.value = "Pineapple";
+  category.value = "Fruit";
   answers.value = answers.value = [...answers.value, "banan"];
   answers.value = answers.value = [...answers.value, "ananas"];
   answers.value = answers.value = [...answers.value, "äpple"];
