6f90c59f6d7ab8a50abe5179b3e12410ec9e1cf8
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-05 11:24:10 +0200

    Add basic code for static quiz question

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <template>
-  <main>
-  </main>
+  <h1>Fruit</h1>
+  <p>Pineapple</p>
+  <AnswerItems :answers="answers" />
+  <p>This was correct</p>
+  <button>Next question</button>
 </template>
 
 <script setup>
+import AnswerItems from "../components/AnswerItems.vue";
+import { ref, onBeforeMount } from "vue";
+const answers = ref([]);
+
+onBeforeMount(() => {
+  answers.value = answers.value = [...answers.value, "banan"];
+  answers.value = answers.value = [...answers.value, "ananas"];
+  answers.value = answers.value = [...answers.value, "äpple"];
+});
 </script>
