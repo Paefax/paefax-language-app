308a0abaef4c16ffa27beffd09127017891ec4bc
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-06 13:01:16 +0200

    Add LandingView

---
+++ src/views/LandingView.vue
@@ -0,0 +1,37 @@
+<template>
+  <main id="landing-page-box">
+    <img
+      id="logo-img"
+      src="../assets/images/logo.png"
+      alt="Paefax logo globe with flags"
+    />
+    <h1 id="landing-page-title">Paefax Language Quiz</h1>
+    <RouterLink to="/quiz">
+      <button>Get Started</button>
+    </RouterLink>
+    <RouterLink to="/account">
+      <button>Already have an account?</button>
+    </RouterLink>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#landing-page-box {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  max-width: 375px;
+}
+
+#logo-img {
+  max-width: 80%;
+}
+
+#landing-page-title {
+  font-size: 1.5em;
+}
+</style>
