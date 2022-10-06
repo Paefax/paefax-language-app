4369a7cd13a4fd9c4036ec3606e5a95d5116e341
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-06 15:43:22 +0200

    Add LanguageView path

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LanguageView from "../views/LanguageView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-  ]
-})
+    {
+      path: "/language",
+      name: "language",
+      component: LanguageView,
+    },
+  ],
+});
 
-export default router
+export default router;
