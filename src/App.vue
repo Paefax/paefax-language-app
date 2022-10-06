fcec3e988b378bda1834dabcb8048db8deba092c
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-06 15:42:13 +0200

    Add flags

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/language">Language</RouterLink>
+    </nav>
   </header>
   <RouterView />
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
