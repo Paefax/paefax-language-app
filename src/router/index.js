df8d3178479f7d9df4fe6a4118b86fdebdec3e67
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-07 11:44:27 +0200

    Add result view

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import QuizView from "../views/QuizView.vue";
 import LanguageView from "../views/LanguageView.vue";
 import LandingView from "../views/LandingView.vue";
 import CategoryView from "../views/CategoryView.vue";
+import ResultView from "../views/ResultView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       path: "/category",
       name: "category",
       component: CategoryView,
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: ResultView,
     },
   ],
 });
---
+++ src/views/ResultView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Good job!</h1>
+  <h2>You finish the quiz</h2>
+  <RouterLink to="/">
+    <button>Home</button>
+  </RouterLink>
+</template>
