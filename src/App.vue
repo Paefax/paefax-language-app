2474987daef9e74fa840e64228118ffb74fc030c
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-07 11:44:15 +0200

    Remove header for now

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,14 @@
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/language">Language</RouterLink>
-      <RouterLink to="/category">Category</RouterLink>
-      <RouterLink to="/quiz">Quiz</RouterLink>
-    </nav>
-  </header>
   <main id="viewport">
     <RouterView />
   </main>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <style scoped>
-header {
-  margin: 0 auto;
-  margin-top: 10px;
-  max-width: 375px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-}
-
-nav {
-  margin: 0 auto;
-}
-
 #viewport {
   margin: 0 auto;
   max-width: 375px;
