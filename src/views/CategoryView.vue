bf394d0cd2a97d0b1d83cd0b257da05734108461
HelenaHalldinITHS <helena.halldin@iths.se>
2022-10-07 11:00:19 +0200

    Add category view

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Choose a category</h1>
+    <section>
+      <CategoryCard
+        v-for="(category, index) in categories"
+        :key="index"
+        :name="category.name"
+      />
+    </section>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CategoryCard from "../components/CategoryCard.vue";
+
+const categories = ref([
+  {
+    name: "Fruit",
+  },
+]);
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
