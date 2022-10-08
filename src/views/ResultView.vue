9191aa314c5bb7ac33957643b970653b18a75220
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-08 14:18:17 +0200

    Add score

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,12 +1,19 @@
 <template>
   <main>
     <h1>Good job!</h1>
-    <h2>You finish the quiz</h2>
+    <h3>Score: {{ quiz.score }} / {{ quiz.numberOfQuestions }}</h3>
+    <h2>You finished the quiz</h2>
     <RouterLink to="/">
       <button>Home</button>
     </RouterLink>
   </main>
 </template>
+
+<script setup>
+import { useQuizStore } from "@/stores/quiz";
+
+const quiz = useQuizStore();
+</script>
 
 <style scoped>
 main {
