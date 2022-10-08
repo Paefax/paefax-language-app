e967e428fb309d4a57abf3ec1763b08ec0d0e962
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-08 16:50:44 +0200

    Add retake function and reset to default

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,12 +1,27 @@
 <template>
   <main>
     <h1>Good job!</h1>
-    <h2>You finish the quiz</h2>
+    <h2>You finished the quiz</h2>
+    <RouterLink to="/retake" v-if="canRetake">
+      <button>Retake</button>
+    </RouterLink>
     <RouterLink to="/">
-      <button>Home</button>
+      <button @click="resetDefault()">Home</button>
     </RouterLink>
   </main>
 </template>
+
+<script setup>
+import { useQuizStore } from "@/stores/quiz";
+
+const quiz = useQuizStore();
+
+const canRetake = quiz.wrongAnsweredQuestions.length > 0 ? true : false;
+
+const resetDefault = () => {
+  quiz.resetToDefault();
+};
+</script>
 
 <style scoped>
 main {
