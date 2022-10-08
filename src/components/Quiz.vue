fa64b2841522c29f26bb5c0d75b6497f293d3ecc
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-08 13:56:13 +0200

    Add Fisher-Yates shuffle algorithm

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -49,6 +49,15 @@ const setQuestionInfo = () => {
     ...answers.value,
     currentQuestion.value.incorrectAnswers[1].word,
   ];
+
+  shuffleAnswers(answers.value);
+};
+
+const shuffleAnswers = (answers) => {
+  for (let i = answers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [answers[i], answers[j]] = [answers[j], answers[i]];
+  }
 };
 
 const nextQuestion = () => {
