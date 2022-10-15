dbe6dc9c829bc0a05b441bcd3646772d8d273444
FelixJacobsen <felix.jacobsen@iths.se>
2022-10-15 18:43:31 +0200

    Design result view after hifi

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
+    <h1>You finished the quiz!</h1>
+    <h2>Score: {{ quiz.score }} / {{ quiz.numberOfQuestions }}</h2>
     <h1>Good job!</h1>
-    <h2>You finish the quiz</h2>
-    <h3>Score: {{ quiz.score }} / {{ quiz.numberOfQuestions }}</h3>
+    <button class="button2">Take new quiz</button>
     <RouterLink to="/category">
-      <button>Choose new category</button>
+      <button class="button1">Change category</button>
     </RouterLink>
-    <RouterLink to="/">
-      <button>Home</button>
+    <RouterLink to="/language">
+      <button class="button1">Change language</button>
     </RouterLink>
   </main>
 </template>
@@ -24,5 +25,32 @@ main {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.button1 {
+  background: #185359;
+  color: #fff;
+  border-radius: 10px;
+  border-style: none;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 500;
+  height: 50px;
+  line-height: 1.5;
+  padding: 14px 40px;
+  transition: all 0.3s;
+}
+.button2 {
+  background: #fff;
+  color: black;
+  border-radius: 10px;
+  border-style: none;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 500;
+  height: 50px;
+  line-height: 1.5;
+  padding: 14px 40px;
+  transition: all 0.3s;
 }
 </style>
