d231ce912759b765aaf30a3ffe339cb7a03c0bf2
FelixJacobsen <felix.jacobsen@iths.se>
2022-10-15 18:56:55 +0200

    Create basic design for the result chart

--- src/components/ResultsChart.vue
+++ src/components/ResultsChart.vue
@@ -1,3 +1,66 @@
 <template>
-  <h2>ResultChart</h2>
+  <table class="content-table">
+    <thead>
+      <tr>
+        <th>Word</th>
+        <th>Your answer</th>
+        <th>Correct answer</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Word1</td>
+        <td>Word1</td>
+        <td>Word1</td>
+      </tr>
+      <tr>
+        <td>Word2</td>
+        <td>Word2</td>
+        <td>Word2</td>
+      </tr>
+      <tr>
+        <td>Word3</td>
+        <td>Word3</td>
+        <td>Word3</td>
+      </tr>
+      <tr>
+        <td>Word4</td>
+        <td>Word4</td>
+        <td>Word4</td>
+      </tr>
+      <tr>
+        <td>Word5</td>
+        <td>Word5</td>
+        <td>Word5</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<style scoped>
+.content-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.content-table thead tr {
+  background-color: #185359;
+  color: #fff;
+  text-align: center;
+}
+
+.content-table th,
+.content-table td {
+  padding: 12px 15px;
+  text-align: center;
+}
+
+.content-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+</style>
