1913abf791ffb2a80219b3a511f493cb582b640d
Philippe <philippe.vial90@gmail.com>
2022-10-17 14:43:42 +0200

    Commit for access

--- src/components/WizardComp.vue
+++ src/components/WizardComp.vue
@@ -1,10 +1,41 @@
 <template>
   <section class="dot-container">
-    <span class="dot"> </span>
-    <span class="dot"> </span>
-    <span class="dot"> </span>
+    <span
+      class="dot"
+      v-for="path in paths"
+      :class="{
+        currentDot: '/' === getPath,
+      }"
+    ></span>
   </section>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+
+const getPath = computed(() => route.path);
+
+const paths = [
+  {
+    id: 0,
+    name: "home",
+    link: "/",
+  },
+  {
+    id: 1,
+    name: "language",
+    link: "/language",
+  },
+  {
+    id: 2,
+    name: "category",
+    link: "/category",
+  },
+];
+</script>
 
 <style scoped>
 .dot-container {
@@ -19,5 +50,11 @@
   background-color: #bbb;
   border-radius: 50%;
   display: inline-block;
+}
+
+.currentDot .routerLink {
+  background: yellow;
+  height: 35px;
+  width: 35px;
 }
 </style>
