6c710ee5d6e861e5623ba2f1921802b3d3bc94d7
Philippe <philippe.vial90@gmail.com>
2022-10-17 11:13:28 +0200

    Get started btn

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -6,7 +6,7 @@
       alt="Official Paefax logo"
     />
     <RouterLink to="/language">
-      <button>Get Started</button>
+      <button class="get-started-btn">Get Started</button>
     </RouterLink>
     <RouterLink to="/login">
       <button>Already have a account?</button>
@@ -29,5 +29,17 @@ main {
 #logo-img {
   max-width: 80%;
   margin-top: 30px;
+}
+.get-started-btn {
+  background: #185359;
+  color: #fff;
+  border: 1px solid black;
+  border-radius: 15px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 500;
+  padding: 10px 20px;
+  transition: all 0.3s;
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 10);
 }
 </style>
