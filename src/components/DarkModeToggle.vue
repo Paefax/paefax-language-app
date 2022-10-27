cb3b914f55a182a37dc32c62bf33d87261ec40ee
ErikssonF <eriksson.nf@gmail.com>
2022-10-27 10:43:18 +0200

    adds darkmode toggle button

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,28 @@
+<template>
+    <main>
+        <button @click="toggleDark" id="darkModeToggle">Change color scheme</button>
+    </main>
+</template>
+
+
+<script setup>
+
+let toggled = true;
+const toggleDark = () => {
+
+    console.log(document.getElementsByClassName("container"));
+    if (toggled) {
+        document.body.style.backgroundColor = "#79A3B1";
+        document.body.style.color = "#000";
+        document.getElementsByClassName("container")[0].style.backgroundColor = "#456268"
+
+        toggled = false;
+    } else {
+        document.body.style.backgroundColor = "rgba(11, 43, 64, 1)"
+        document.body.style.color = "#FFFF"
+        document.getElementsByClassName("container")[0].style.backgroundColor = "#185359"
+
+        toggled = true;
+    }
+};
+</script>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,4 +1,9 @@
 <template>
   <h1>Settings</h1>
-  <h3>There currently isn't any settings</h3>
+  <h3>Try out our different color schemes!</h3>
+  <DarkModeToggle />
 </template>
+
+<script setup>
+import DarkModeToggle from '../components/DarkModeToggle.vue'
+</script>
