1c9ab1051d983b217a2157b1ce77306a04fdb09a
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-10-27 10:59:48 +0200

    Add basic styling

--- src/views/MakeQuizView.vue
+++ src/views/MakeQuizView.vue
@@ -29,28 +29,31 @@
         />
         <label for="answer">Answer</label>
         <input type="text" id="answer" name="answer" v-model="answerToAdd" />
-        <button @click.prevent="addQuestion()">ADD</button>
+        <button id="add-button" @click.prevent="addQuestion()">ADD</button>
       </form>
+      <section id="questions-line">
+        <div class="line"></div>
+        <h3>Questions</h3>
+        <div class="line"></div>
+      </section>
       <section id="added-questions-section">
-        <section id="questions-line">
-          <div class="line"></div>
-          <h3>Questions</h3>
-          <div class="line"></div>
-        </section>
         <section>
           <table id="added-questions-table" v-if="addedQuestions.length > 0">
             <thead>
-              <th>ID</th>
-              <th>Question</th>
-              <th>Answer</th>
-              <th></th>
+              <th id="id-header">ID</th>
+              <th id="question-header">Question</th>
+              <th id="answer-header">Answer</th>
+              <th id="empty-header"></th>
             </thead>
             <tbody v-for="(question, index) in addedQuestions" :key="index">
               <td>{{ index }}</td>
               <td>{{ question.question }}</td>
               <td>{{ question.correctAnswer }}</td>
-              <td>
-                <button id="remove" @click.prevent="deleteQuestion(index)">
+              <td id="delete-column">
+                <button
+                  id="remove-button"
+                  @click.prevent="deleteQuestion(index)"
+                >
                   X
                 </button>
               </td>
@@ -123,11 +126,10 @@ main {
   display: flex;
   flex-direction: column;
   max-width: 500px;
+  font-size: 0.8em;
 }
 
 h1 {
-  margin-left: 100px;
-  margin-right: 100px;
   align-self: center;
 }
 
@@ -136,7 +138,7 @@ form {
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  gap: 10px;
+  gap: 12px;
   margin: auto;
 }
 
@@ -145,16 +147,31 @@ h2 {
   align-self: center;
 }
 
-#added-questions-section {
+input,
+select {
   width: 100%;
+  height: 30px;
+  border-radius: 10px;
+  background-color: rgb(255, 255, 255, 0.56);
+  border-style: none;
+  font-size: 1.2em;
+  padding-left: 10px;
+}
+
+#added-questions-section {
+  max-width: 375px;
+  width: 90%;
+  align-self: center;
 }
 
 #questions-line {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  align-self: center;
   gap: 5px;
   margin-top: 20px;
+  min-width: 90vw;
 }
 
 .line {
@@ -163,8 +180,73 @@ h2 {
   height: 0;
 }
 
+#added-questions-table {
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 100%;
+}
+
+td {
+  border-top: 1px solid white;
+  word-wrap: break-word;
+}
+
+th,
+td {
+  border-right: 1px solid white;
+  padding: 8px;
+}
+
+#question-header {
+  width: 110px;
+}
+
+#answer-header {
+  width: 110px;
+}
+
+#empty-header,
+#delete-column {
+  border-right: none;
+}
+
+button {
+  border-radius: 10px;
+  border-style: none;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 15px;
+  transition: all 0.3s;
+}
+
+#add-button {
+  background: #185359;
+  color: #fff;
+  margin-top: 5px;
+  font-size: 0.9em;
+}
+
+#remove-button {
+  font-size: 1em;
+  padding: 5px 10px;
+}
+
 #create-button {
   align-self: center;
   margin-top: 30px;
+  margin-bottom: 50px;
+  background: #185359;
+  color: #fff;
+  font-size: 1.2em;
+}
+
+@media only screen and (min-width: 376px) {
+  main {
+    font-size: 1em;
+  }
+
+  #questions-line {
+    min-width: 375px;
+  }
 }
 </style>
